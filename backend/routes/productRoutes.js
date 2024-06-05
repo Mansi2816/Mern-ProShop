3fdb7a86fe0b8cc87fba49e17b26295965c2e4c3
Mansi2816 <mansisoni785@gmail.com>
2024-06-05 23:51:17 +0530

    error handling

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,10 +5,10 @@ const asyncHandler = require('../middleware/async-Handler')
  const Product = require('../models/productModel')  
 
 
-
 router.get('/', asyncHandler (async(req,res) => {
     
     const products = await Product.find({})    
+    
     res.json(products)
     }))
     
@@ -18,7 +18,7 @@ router.get('/', asyncHandler (async(req,res) => {
      if(product) {
        return res.json(product)
      }
-     res.status(404).json({message: 'Product not found'})
+     res.status(404).json({message: 'Resource not found'})
 
     }))
 
