c7809a2896aaa3b120c64cef727ac84605b7c110
Mansi Soni <mansisoni785@gmail.com>
2024-06-05 11:32:04 +0530

    change href to LinkContainer

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,27 +1,43 @@
 import React from 'react'
-import {Navbar,Nav,Container} from 'react-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
 import logo from '../assets/logo.png'
+import { LinkContainer } from 'react-router-bootstrap'
 
 const Header = () => {
   return (
     <header>
-<Navbar bg='dark' variant='dark' expand="md" collapseOnSelect>
-    <Container>
-        <Navbar.Brand href='/'>
-            <img src={logo} alt='ProShop' />
-            ProShop</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-                <Nav.Link href='/'>Home</Nav.Link>
-                <Nav.Link href='/cart'><FaShoppingCart /> Cart</Nav.Link>
-                <Nav.Link href='/login'><FaUser /> Login</Nav.Link>
-            </Nav>
-        </Navbar.Collapse>
-    </Container>
-</Navbar>
+      <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>
+              <img src={logo} alt='ProShop' />
+              ProShop
+            </Navbar.Brand>
+          </LinkContainer>
 
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='me-auto'>
+
+              <LinkContainer to='/cart'>
+              <Nav.Link>
+                  <FaShoppingCart /> Cart
+                  </Nav.Link>
+              </LinkContainer>
+
+
+              <LinkContainer to='/login'>
+
+              <Nav.Link>
+                <FaUser /> Login
+                </Nav.Link>
+                </LinkContainer>   
+                         
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
   )
 }
