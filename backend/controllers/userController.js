613fe1d69d280e5abdb36b695cfdbfb0c0811a95
Mansi Soni <mansisoni785@gmail.com>
2024-06-19 19:10:10 +0530

    backend user routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -127,7 +127,7 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 //@access Private/Admin
 const getUsers = asyncHandler(async (req, res) => {
   const users = await User.find({});
-  res.json(users);
+  res.status(200).json(users);
 });
 
 //@desc Delete user
@@ -136,15 +136,19 @@ const getUsers = asyncHandler(async (req, res) => {
 const deleteUser = asyncHandler(async (req, res) => {
   const user = await User.findById(req.params.id);
 
-  if (user) {
-    await user.deleteOne();
-    res.json({ message: 'User removed' });
-  } else {
-    res.status(404);
-    throw new Error('User not found');
+if(user) {
+  if (user.isAdmin) {
+    res.status(400)
+    throw new Error('cannot delete admin user')
   }
-});
-
+  await User.deleteOne({_id: user._id})
+  res.status(200).json({message: 'User deleted successfully'})
+}
+else{
+  res.status(404)
+  throw new Error('User not found')
+}
+})
 //@desc Update user
 //@route PUT/api/users/:id
 //@access Private/Admin
@@ -154,16 +158,17 @@ const updateUser = asyncHandler(async (req, res) => {
   if (user) {
     user.name = req.body.name || user.name;
     user.email = req.body.email || user.email;
-    user.isAdmin = req.body.isAdmin === undefined ? user.isAdmin : req.body.isAdmin;
+    user.isAdmin = Boolean(req.body.isAdmin)
 
     const updatedUser = await user.save();
 
-    res.json({
+    res.status(200).json({
       _id: updatedUser._id,
       name: updatedUser.name,
       email: updatedUser.email,
       isAdmin: updatedUser.isAdmin,
     });
+
   } else {
     res.status(404);
     throw new Error('User not found');
@@ -174,15 +179,11 @@ const updateUser = asyncHandler(async (req, res) => {
 //@route GET/api/users/:id
 //@access Private/Admin
 const getUsersbyID = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.params.id);
+  const user = await User.findById(req.params.id).select('-password');
 
   if (user) {
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-    });
+    res.status(200).json(user);
+    
   } else {
     res.status(404);
     throw new Error('User not found');
